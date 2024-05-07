d1b7aae1ab54140168bbdf3d077998ab73e3eb73
Raphael Feitosa Assumpção <raphaelassumpcao00@gmail.com>
2024-05-07 16:07:58 -0300

    Crie um projeto onde mostre o número antecessor e sucessor de um número digitado

---
+++ Ex004.py
@@ -0,0 +1,4 @@
+#Crie um projeto onde mostre o número antecessor e sucessor de um número digitado
+n = int(input('Digite um número: '))
+print('O número é {}, seu antecessor é {} e seu sucessor é {}'.format(n,(n-1),(n+1)))
+
