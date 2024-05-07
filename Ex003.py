7e57139027e095add138c2e717ec38e954738cd2
Raphael Feitosa Assumpção <raphaelassumpcao00@gmail.com>
2024-05-07 16:05:37 -0300

    Crie um projeto onde é realizado a soma de dois números escolhidos pelo usuário

---
+++ Ex003.py
@@ -0,0 +1,5 @@
+#Crie um projeto onde é realizado a soma de dois números escolhidos pelo usuário
+n1 = int(input('Digite o número: '))
+n2 = int(input('Digite o segundo número: '))
+s = n1 + n2
+print('A soma entre {0} e {1} vale {2}'.format(n1,n2,s))
