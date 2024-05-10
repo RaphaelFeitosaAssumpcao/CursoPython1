f7d59e9d75d7e2a9507efb135cae7a1ae66e33df
Raphael Feitosa Assumpção <raphaelassumpcao00@gmail.com>
2024-05-09 22:49:21 -0300

    Crie um projeto onde mostre seu dinheiro e o quanto você consegue ter em dolar

---
+++ Ex008.py
@@ -0,0 +1,4 @@
+#Crie um projeto onde mostre seu dinheiro e o quanto você consegue ter em dolar
+real = float(input('Digite quanto de dinheiro você tem R$: '))
+dolar = real/5.16
+print('Você pode comprar US$ {:.2f}'.format(dolar))
