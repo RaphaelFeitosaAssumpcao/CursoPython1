e86f4f521ace36dc7bbc6a1742cf299625bd0208
Raphael Feitosa Assumpção <raphaelassumpcao00@gmail.com>
2024-05-09 22:50:49 -0300

    Crie um projeto onde mostre a dimensão e a sua área de uma parede

---
+++ Ex009.py
@@ -0,0 +1,4 @@
+largura = float(input('Digite a largura da parede: '))
+altura = float(input('Digite a altura da parede: '))
+area = largura*altura
+print ('Sua parede tem dimensão de {} X {} e sua área é de {}m²'.format(largura,altura,area))
