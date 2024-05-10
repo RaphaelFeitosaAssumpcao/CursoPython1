9f0252191d239f105f0fce50e241eed1cc8b4f18
Raphael Feitosa Assumpção <raphaelassumpcao00@gmail.com>
2024-05-09 22:58:23 -0300

    Crie um programa onde mostre um número float e a sua porção inteira.

---
+++ Ex012.py
@@ -0,0 +1,7 @@
+#Crie um programa onde mostre um número float e a sua porção inteira.
+'''from math import trunc
+num = float(input('Digite um valor float: '))
+print('O valor digitado foi {} e sua porção inteira é {}'.format(num,trunc(num)))'''
+
+num = float(input('Digite um valor float: '))
+print('O valor float do número é {} e a porção inteira é {}'.format(num,int(num)))
